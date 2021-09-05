304d3162d69eecfb6fa5f88c6c52a27d6b105c7c
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-09-06 00:32:23 +0430

    fixed some bugs

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -19,10 +19,15 @@ public class MapManager : MonoBehaviour
     private void Awake()
     {
         _abstractMap = FindObjectOfType<AbstractMap>();
-        _quadTreeCameraMovement = FindObjectOfType<QuadTreeCameraMovement>();
+        _quadTreeCameraMovement = FindObjectOfType<QuadTreeCameraMovement>();       
+    }
 
-        int index = PlayerPrefs.GetInt("LastMapTypeIndex", 0);
-        SetMapImagerySourceType(index);
+    private void Start()
+    {
+        int lastMapTypeIndex = PlayerPrefs.GetInt("LastMapTypeIndex", 0);
+        SetMapImagerySourceType(lastMapTypeIndex);
+
+        InitializeMapZoomAmount();
 
         _quadTreeCameraMovement.SetPanSpeed(PanSpeed);
         _quadTreeCameraMovement.SetZoomSpeed(ZoomSpeed);
@@ -40,12 +45,25 @@ public class MapManager : MonoBehaviour
         IsMapTypeSelectedGameObjects[index].SetActive(true);
     }
 
+    private void InitializeMapZoomAmount()
+    {
+        int mapZoomIndex = PlayerPrefs.GetInt("MapZoomIndex", 0);
+        _currnetZoomAmountIndex = mapZoomIndex;
+        _abstractMap.SetZoom(_possibleZoomAmounts[_currnetZoomAmountIndex]);
+    }
+
+    private void SetMapZoom(int index)
+    {
+        _currnetZoomAmountIndex = index;
+        _abstractMap.UpdateMap(_possibleZoomAmounts[_currnetZoomAmountIndex]);
+        PlayerPrefs.SetInt("MapZoomIndex", index);
+    }
+
     public void ZoomIn()
     {
         if (_currnetZoomAmountIndex < 4)
         {
-            _currnetZoomAmountIndex++;
-            _abstractMap.UpdateMap(_possibleZoomAmounts[_currnetZoomAmountIndex]);
+            SetMapZoom(_currnetZoomAmountIndex + 1);
         }
     }
 
@@ -53,8 +71,7 @@ public class MapManager : MonoBehaviour
     {
         if (_currnetZoomAmountIndex > 0)
         {
-            _currnetZoomAmountIndex--;
-            _abstractMap.UpdateMap(_possibleZoomAmounts[_currnetZoomAmountIndex]);
+            SetMapZoom(_currnetZoomAmountIndex - 1);
         }
     }
 }
