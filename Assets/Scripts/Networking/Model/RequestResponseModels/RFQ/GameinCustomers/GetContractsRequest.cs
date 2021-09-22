d872faa684d96242a41ade0b7f195cc7c043c935
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-09-22 16:05:03 +0330

    fixed merger conflicts

--- Assets/Scripts/Networking/Model/RequestResponseModels/RFQ/GameinCustomers/GetContractsRequest.cs
+++ Assets/Scripts/Networking/Model/RequestResponseModels/RFQ/GameinCustomers/GetContractsRequest.cs
@@ -1,13 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class GetContractsRequest : RequestObject
 {
-    public GetContractsRequest(RequestTypeConstant requestTypeConstant)
+    public GetContractsRequest(RequestTypeConstant requestTypeConstant) : base(requestTypeConstant)
     {
-        this.playerId = PlayerPrefs.GetInt("PlayerId", 0);
-        this.requestTypeConstant = Convert.ToInt32(requestTypeConstant);
+
     }
 }
