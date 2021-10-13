0ec412dd05ff0662bc06938d961336964dd06aa0
karaneh keypoor <keypoor.karaneh@gmail.com>
2021-10-14 00:39:46 +0330

    added auctions popup in markers prefab and the scripts

---
+++ Assets/Scripts/Map/OnMapMarkersManager.cs
@@ -0,0 +1,35 @@
+﻿
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnMapMarkersManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.Instance.OnChangeMapMarkerResponseEvent += OnChangeMapMarkerResponseReceive;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.Instance.OnChangeMapMarkerResponseEvent -= OnChangeMapMarkerResponseReceive;
+    }
+    
+    public void OnChangeMapMarkerResponseReceive(ChangeMapMarkerResponse changeMapMarkerResponse)
+    {
+        if (changeMapMarkerResponse.result == "Successful")
+        {
+            int playerId = PlayerPrefs.GetInt("PlayerId");
+            if (changeMapMarkerResponse.bidderPlayerId == playerId)
+            {
+                //TODO show feedback for successfully biding higher
+            }
+            //TODO set the new values for the changed on map marker
+        }
+        else
+        {
+            //TODO show feedback for the unsuccessful bid
+        }
+    }
+    
+}
