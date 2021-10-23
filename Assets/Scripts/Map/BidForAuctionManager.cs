283dc632a029568aca0ce5989f5f4cf495295327
karaneh keypoor <keypoor.karaneh@gmail.com>
2021-10-23 16:31:40 +0330

    added visual feedback for bid results

---
+++ Assets/Scripts/Map/BidForAuctionManager.cs
@@ -0,0 +1,50 @@
+﻿
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BidForAuctionManager : MonoBehaviour
+{
+    public GameObject bidResultPopUp;
+    public GameObject successfulResult;
+    public GameObject unsuccessfulResult;
+    
+    private void OnEnable()
+    {
+        EventManager.Instance.OnBidForAuctionResponseEvent += OnBidForAuctionResponseReceive;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.Instance.OnBidForAuctionResponseEvent -= OnBidForAuctionResponseReceive;
+    }
+
+    private void OnBidForAuctionResponseReceive(BidForAuctionResponse bidForAuctionResponse)
+    {
+        if (bidForAuctionResponse.result == "success")
+        {
+            int teamId = PlayerPrefs.GetInt("TeamId");
+            if (bidForAuctionResponse.auction.highestBidTeamId == teamId)
+            {
+                if (SceneManager.GetActiveScene().name == "MapScene")
+                {
+                    successfulResult.SetActive(true);
+                    unsuccessfulResult.SetActive(false);
+                    bidResultPopUp.SetActive(true);
+                }
+            }
+            GameDataManager.Instance.UpdateAuctionElement(bidForAuctionResponse.auction);
+            MapManager.Instance.UpdateAllOnMapMarkers();
+        }
+        else
+        {
+            if (SceneManager.GetActiveScene().name == "MapScene")
+            {
+                successfulResult.SetActive(false);
+                unsuccessfulResult.SetActive(true);
+                bidResultPopUp.SetActive(true);
+            }
+        }
+    }
+    
+}
