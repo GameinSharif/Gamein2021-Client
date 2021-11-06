7b6402ef18d1771fd63660757bf0fbe01ce5f54c
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-11-06 20:48:46 +0330

    added LetterBoxer

--- Assets/Scripts/Map/OnMapMarkerCanvasSetter.cs
+++ Assets/Scripts/Map/OnMapMarkerCanvasSetter.cs
@@ -6,14 +6,8 @@ public class OnMapMarkerCanvasSetter : MonoBehaviour
 {
     public Canvas canvas;
 
-    private void OnMouseDown()
-    {
-        Debug.Log(1);
-    }
-
     private void Start()
     {
-        canvas = GetComponent<Canvas>();
         canvas.worldCamera = Camera.allCameras[0];
         canvas.sortingOrder = 1;
     }
