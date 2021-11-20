bf4a165728dc21c7c6df4f215ae8392d2945ed8b
karaneh keypoor <keypoor.karaneh@gmail.com>
2021-11-20 20:49:36 +0330

    add product suppliers popup and suppliers canvas prefab

--- Assets/Scripts/RFQ/Gamein Suppliers/RawProductItemController.cs
+++ Assets/Scripts/RFQ/Gamein Suppliers/RawProductItemController.cs
@@ -9,15 +9,10 @@ public class RawProductItemController : MonoBehaviour
     public Localize productNameLocalize;
 
     public GameObject ShowSuppliesButtonGameObject;
-    public GameObject HideSuppliesButtonGameObject;
-    public GameObject SuppliesParent;
-    public GameObject SupplyItemsParent;
 
-    public GameObject SupplyItemPrefab;
     
     private Utils.Product _rawProduct;
     private List<Utils.WeekSupply> _supplies;
-    private List<GameObject> _spawnedSupplyItemGameObjects = new List<GameObject>();
 
     public void SetInfo(int no, string productName)
     {
@@ -35,69 +30,11 @@ public class RawProductItemController : MonoBehaviour
         );
         
         ShowSuppliesButtonGameObject.SetActive(true);
-        HideSuppliesButtonGameObject.SetActive(false);
-        SetSupplies();
-        SuppliesParent.SetActive(false);
     }
-
-    public void SetSupplies()
-    {
-        _supplies = GameDataManager.Instance.GetCurrentWeekRawProductSupplies(_rawProduct.id);
-        DeactiveAllChildrenInScrollPanel();
-        for (int i = 0; i < _supplies.Count; i++)
-        {
-            AddContractDetailItemToList(_supplies[i], i + 1);
-        }
-    }
-
-    private void DeactiveAllChildrenInScrollPanel()
-    {
-        foreach (GameObject gameObject in _spawnedSupplyItemGameObjects)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-    
-    private void AddContractDetailItemToList(Utils.WeekSupply supply, int index)
-    {
-        GameObject createdItem = GetItem(SupplyItemsParent);
-        createdItem.transform.SetSiblingIndex(index);
-
-        RawProductSupplyItemController controller = createdItem.GetComponent<RawProductSupplyItemController>();
-        controller.SetInfo(index, supply);
-
-        createdItem.SetActive(true);
-    }
-
-    private GameObject GetItem(GameObject parent)
-    {
-        foreach (GameObject gameObject in _spawnedSupplyItemGameObjects)
-        {
-            if (!gameObject.activeSelf)
-            {
-                return gameObject;
-            }
-        }
-
-        GameObject newItem;
-        newItem = Instantiate(SupplyItemPrefab, parent.transform);
-        _spawnedSupplyItemGameObjects.Add(newItem);
-        return newItem;
-    }
-
     
     public void OnShowSuppliesButtonClicked()
     {
-        SuppliesParent.SetActive(true);
-        ShowSuppliesButtonGameObject.SetActive(false);
-        HideSuppliesButtonGameObject.SetActive(true);
+        RawProductSuppliersPopupController.Instance.OnShowSuppliersClick(_rawProduct);
     }
-
-    public void OnHideSuppliesButtonClicked()
-    {
-        SuppliesParent.SetActive(false);
-        ShowSuppliesButtonGameObject.SetActive(true);
-        HideSuppliesButtonGameObject.SetActive(false);
-    }
-    
+   
 }
---
+++ Assets/Scripts/RFQ/Gamein Suppliers/RawProductSuppliersPopupController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Linq;
+using RTLTMPro;
+using UnityEngine.UI;
+
+public class RawProductSuppliersPopupController : MonoBehaviour
+{
+    public static RawProductSuppliersPopupController Instance;
+
+    public GameObject rawProductSuppliersPopupCanvas;
+    public GameObject rawProductSuppliersScrollViewParent;
+
+    public GameObject supplyItemPrefab;
+
+    public Image productImage;
+    public Localize productNameLocalize;
+
+    private Utils.Product _product;
+    private List<Utils.WeekSupply> _supplies;
+    private List<GameObject> _spawnedSupplyItemGameObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnShowSuppliersClick(Utils.Product product)
+    {
+        this._product = product;
+        
+        productNameLocalize.SetKey("product_" + product.name);
+        productImage.sprite = GameDataManager.Instance.ProductSprites[product.id - 1];
+        SetSupplies();
+        rawProductSuppliersPopupCanvas.SetActive(true);
+    }
+      
+    private void SetSupplies()
+    {
+        _supplies = GameDataManager.Instance.GetCurrentWeekRawProductSupplies(_product.id);
+        DeactiveAllChildrenInScrollPanel();
+        for (int i = 0; i < _supplies.Count; i++)
+        {
+            AddSupplierItemToList(_supplies[i], i + 1);
+        }
+    }
+
+    private void DeactiveAllChildrenInScrollPanel()
+    {
+        foreach (GameObject gameObject in _spawnedSupplyItemGameObjects)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+    private void AddSupplierItemToList(Utils.WeekSupply supply, int index)
+    {
+        GameObject createdItem = GetItem(rawProductSuppliersScrollViewParent);
+        createdItem.transform.SetSiblingIndex(index);
+
+        RawProductSupplyItemController controller = createdItem.GetComponent<RawProductSupplyItemController>();
+        controller.SetInfo(index, supply);
+
+        createdItem.SetActive(true);
+    }
+
+    private GameObject GetItem(GameObject parent)
+    {
+        foreach (GameObject gameObject in _spawnedSupplyItemGameObjects)
+        {
+            if (!gameObject.activeSelf)
+            {
+                return gameObject;
+            }
+        }
+
+        GameObject newItem;
+        newItem = Instantiate(supplyItemPrefab, parent.transform);
+        _spawnedSupplyItemGameObjects.Add(newItem);
+        return newItem;
+    }
+
+
+
+}
