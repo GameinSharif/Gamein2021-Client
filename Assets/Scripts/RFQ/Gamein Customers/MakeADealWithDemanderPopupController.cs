46a2ebb76f658a51ee8efeaf3fda297f0396735d
karaneh keypoor <keypoor.karaneh@gmail.com>
2021-11-24 05:13:54 +0330

    add part of gamein customers UI

---
+++ Assets/Scripts/RFQ/Gamein Customers/MakeADealWithDemanderPopupController.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using RTLTMPro;
+using UnityEngine.UI;
+
+public class MakeADealWithDemanderPopupController : MonoBehaviour
+{
+    public static MakeADealWithDemanderPopupController Instance;
+
+    public GameObject makeADealWithDemanderPopupCanvas;
+
+    public Image productImage;
+    public Localize productNameLocalize;
+    public TMP_InputField amount;
+    public RTLTextMeshPro pricePerUnit;
+    public TMP_InputField totalPrice;
+    public TMP_InputField finalPrice;
+    public TMP_InputField arrivalDate;
+    public ToggleGroup transportationMode;
+    public TMP_InputField numberOfRepetition;
+    public TMP_InputField penalty; //TODO calculate?
+    public ToggleGroup wantsInsurance;
+
+    private Utils.WeekDemand _weekDemand;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.Instance.OnNewContractSupplierResponseEvent += OnNewContractSupplierResponse;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.Instance.OnNewContractSupplierResponseEvent -= OnNewContractSupplierResponse;
+    }
+
+    private void OnNewContractSupplierResponse(NewContractSupplierResponse newContractSupplierResponse)
+    {
+        if (newContractSupplierResponse.result == "Successful")
+        {
+            List<Utils.ContractSupplier> contractSuppliers = newContractSupplierResponse.contractSuppliers;
+            GameinSuppliersController.Instance.AddContractItemsToList(contractSuppliers);
+            Utils.ContractSupplier firstContract = contractSuppliers[0];
+            float cost = firstContract.boughtAmount * firstContract.pricePerUnit + firstContract.transportationCost;
+            MainHeaderManager.Instance.Money = MainHeaderManager.Instance.Money - cost;
+            makeADealWithDemanderPopupCanvas.SetActive(false);
+        }
+        else
+        {
+            DialogManager.Instance.ShowErrorDialog();
+        }
+    }
+
+    public void OnOpenMakeADealPopupClick(Utils.WeekDemand weekSupply)
+    {
+        _weekDemand = weekSupply;
+        
+        //TODO clear inputfields
+        
+        Utils.Product product = GameDataManager.Instance.GetProductById(weekSupply.productId);
+        productNameLocalize.SetKey("product_" + product.name);
+        //pricePerUnit.text = weekSupply.price + "$";
+        productImage.sprite = GameDataManager.Instance.ProductSprites[product.id - 1];
+        CustomDate date = MainHeaderManager.Instance.gameDate.AddDays(GetTransportDuration());
+        arrivalDate.text = date.ToString();
+        makeADealWithDemanderPopupCanvas.SetActive(true);
+    }
+
+    private int GetTransportDuration()
+    {
+        Utils.VehicleType vehicleType = GetTransportationMode();
+        Vector2 destinationLocation = GameDataManager.Instance.GetMyTeamLocaionOnMap();
+        Vector2 sourceLocation = GameDataManager.Instance.GetLocationByTypeAndId(Utils.TransportNodeType.GAMEIN_CUSTOMER, _weekDemand.gameinCustomerId);
+        int duration = TransportManager.Instance.CalculateTransportDuration(sourceLocation, destinationLocation, vehicleType);
+        return duration;
+    }
+    
+    public void OnAmountValueChange()
+    {
+        string amount = this.amount.text;
+        if (string.IsNullOrEmpty(amount))
+        {
+            return;
+        }
+
+        //int total = int.Parse(amount) * _weekDemand.price;
+        float transportationCost = GetTransportCost(int.Parse(amount));
+        //float final = total + transportationCost;
+        //totalPrice.text = total.ToString("0.00") + "$";
+        //finalPrice.text = final.ToString("0.00") + "$";
+    }
+
+    private float GetTransportCost(int amount)
+    {
+        Utils.VehicleType vehicleType = GetTransportationMode();
+        Vector2 destinationLocation = GameDataManager.Instance.GetMyTeamLocaionOnMap();
+        Vector2 sourceLocation = GameDataManager.Instance.GetLocationByTypeAndId(Utils.TransportNodeType.GAMEIN_CUSTOMER, _weekDemand.gameinCustomerId);
+        int distance = TransportManager.Instance.GetTransportDistance(sourceLocation, destinationLocation, vehicleType);
+        float cost = TransportManager.Instance.CalculateTransportCost(vehicleType, distance, _weekDemand.productId, amount, WantsInsurance());
+        return cost;
+    }
+
+    private bool WantsInsurance()
+    {
+        Toggle wants = wantsInsurance.ActiveToggles().FirstOrDefault();
+        if (wants.name == "Yes")
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private Utils.VehicleType GetTransportationMode()
+    {
+        Toggle mode = transportationMode.ActiveToggles().FirstOrDefault();
+        switch (mode.name)
+        {
+            case "AirPlane":
+                return Utils.VehicleType.AIRPLANE;
+            case "Train":
+                return Utils.VehicleType.TRAIN;
+            case "Truck":
+                return Utils.VehicleType.TRUCK;
+            case "Vanet":
+                return Utils.VehicleType.VANET;
+            default:
+                return Utils.VehicleType.AIRPLANE;
+        }
+    }
+
+    public void OnTransportationModeToggleChange()
+    {
+        OnAmountValueChange();
+        CustomDate date = MainHeaderManager.Instance.gameDate.AddDays(GetTransportDuration());
+        arrivalDate.text = date.ToString();
+    }
+
+    public void OnInsuranceToggleChange()
+    {
+        OnAmountValueChange();
+    }
+    
+    private int GetRepetitionWeeks()
+    {
+        string weeks = numberOfRepetition.text;
+        if (string.IsNullOrEmpty(weeks))
+        {
+            return 0;
+        }
+        int weeksInt = int.Parse(numberOfRepetition.text);
+        if (weeksInt < 0)
+        {
+            return -1;
+        }
+        return weeksInt;
+    }
+
+    private bool CanAffordMakingContract()
+    {
+        //TODO
+        float currentMoney = MainHeaderManager.Instance.Money;
+        int total = int.Parse(amount.text);
+        float transportationCost = GetTransportCost(int.Parse(amount.text));
+        float final = total + transportationCost;
+
+        return final <= currentMoney;
+    }
+
+    private bool StorageHasCapacity()
+    {
+        int amount = int.Parse(this.amount.text);
+        Utils.Product product = GameDataManager.Instance.GetProductById(_weekDemand.productId);
+        int neededCapacity = amount * product.volumetricUnit;
+        int teamId = PlayerPrefs.GetInt("TeamId");
+        Utils.Factory factory = GameDataManager.Instance.GetFactoryById(GameDataManager.Instance.GetTeamById(teamId).factoryId);
+        Utils.Storage storage = StorageManager.Instance.GetStorageByBuildingIdAndType(factory.id, false);
+        int capacity = StorageManager.Instance.CalculateAvailableCapacity(storage, Utils.ProductType.RawMaterial, true);
+        return neededCapacity <= capacity;
+    }
+    
+    public void OnDoneButtonClick()
+    {
+        Debug.LogWarning(1);
+        string amountText = amount.text;
+        Utils.VehicleType vehicleType = GetTransportationMode();
+        int weeks = GetRepetitionWeeks();
+        if (weeks < 0 || string.IsNullOrEmpty(amountText))
+        {
+            DialogManager.Instance.ShowErrorDialog("empty_input_field_error");
+            return;
+        }
+        Debug.LogWarning(2);
+        Debug.LogWarning(vehicleType);
+        if (CanAffordMakingContract() && StorageHasCapacity())
+        {
+            int amountInt = int.Parse(amount.text);
+            NewContractRequest newContractSupplier = new NewContractRequest(RequestTypeConstant.NEW_CONTRACT, _weekDemand.gameinCustomerId, _weekDemand.productId, amountInt,
+                int.Parse(pricePerUnit.text), weeks);
+            RequestManager.Instance.SendRequest(newContractSupplier);
+        }
+        if (!CanAffordMakingContract())
+        {
+            DialogManager.Instance.ShowErrorDialog("not_enough_money_error");
+        }
+
+        if (!StorageHasCapacity())
+        {
+            DialogManager.Instance.ShowErrorDialog("not_enough_capacity_error");
+
+        }
+    }
+}
