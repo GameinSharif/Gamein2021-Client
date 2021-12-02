ffeab378fcac4d1a9013478c12d60792b81e9dbf
Meysam Bavi <meybsky@gmail.com>
2021-12-03 03:26:39 +0330

    Added initial code

---
+++ Assets/Scripts/Transport/TransportsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportsController : MonoBehaviour
+{
+    public static TransportsController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public List<Sprite> markerSprites;
+    public List<Sprite> vehicleSprites;
+    
+    public Sprite GetSpriteByAgentType(MapUtils.MapAgentMarker.AgentType agentType)
+    {
+        //TODO
+        return null;
+    }
+
+    public Sprite GetVehicleSprite(Utils.VehicleType transportVehicleType)
+    {
+        return vehicleSprites[(int) transportVehicleType];
+    }
+}
