8ff513003c851a654c9bd51cedf3c56bf7201d41
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2021-12-12 00:11:28 +0330

    -fix map bug (click on UI)

--- Assets/Scripts/Map/ClickOnMapMarkerHandler.cs
+++ Assets/Scripts/Map/ClickOnMapMarkerHandler.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ClickOnMapMarkerHandler : MonoBehaviour
 {
     private void OnMouseDown()
     {
+        if(EventSystem.current.IsPointerOverGameObject()) return;
         //TODO conditions for all map marker types
         GetComponentInParent<EachAuctionController>().OnFactoryButtonClick();
         GetComponentInParent<EachDcController>().OnDcMarkerClicked();
