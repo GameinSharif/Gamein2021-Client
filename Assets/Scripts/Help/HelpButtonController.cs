46f571e8c86c8ba6a2baf052e0c24097e0cdcda3
Meysam Bavi <meybsky@gmail.com>
2021-12-11 16:48:08 +0330

    Added initial code for help

---
+++ Assets/Scripts/Help/HelpButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpButtonController : MonoBehaviour
+{
+    public GameObject helpFramePrefab;
+    public string localizeKey;
+    public Transform parent;
+
+    private GameObject _frame;
+
+    private void OnMouseEnter()
+    {
+        if (_frame == null)
+        {
+            _frame = Instantiate(helpFramePrefab, parent, true);
+            
+            var buttonTransform = this.transform;
+            _frame.transform.SetPositionAndRotation(buttonTransform.position, buttonTransform.rotation);
+            _frame.transform.localScale = Vector3.one;
+            
+            _frame.GetComponent<HelpFrameController>().SetText(localizeKey);
+        }
+        _frame.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _frame.SetActive(false);
+    }
+}
